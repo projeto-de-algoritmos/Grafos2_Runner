db6a4b7b429a76fe0d16f38edcba8f4fa93d2eea
Guilherme Mendes <guimendesp12@gmail.com>
2020-10-01 00:31:18 -0300

    Add initial implementation Dijkstra

--- src/game.py
+++ src/game.py
@@ -405,8 +405,45 @@ def stamina_regen(player):
 def min_dist(machine, player, out):
     while math.hypot(player.position[0] - out.position[0], player.position[1] - out.position[1]) < 300:
         player.position = random_pos()
-    while math.hypot(machine.position[0] - out.position[0], machine.position[1] - out.position[1]) < math.hypot(player.position[0] - out.position[0], player.position[1] - out.position[1]):
+    while math.hypot(machine.position[0] - out.position[0], machine.position[1] - out.position[1]) < math.hypot(
+            player.position[0] - out.position[0], player.position[1] - out.position[1]):
         machine.position = random_pos()
+
+
+def dijsktra(graph, initial, end):
+    shortest_paths = {initial: (None, 0)}
+    current_node = initial
+    visited = set()
+
+    while current_node != end:
+        visited.add(current_node)
+        destinations = graph.edges[current_node]
+        weight_to_current_node = shortest_paths[current_node][1]
+
+        for next_node in destinations:
+            weight = graph.weights[(current_node, next_node)] + weight_to_current_node
+            if next_node not in shortest_paths:
+                shortest_paths[next_node] = (current_node, weight)
+            else:
+                current_shortest_weight = shortest_paths[next_node][1]
+                if current_shortest_weight > weight:
+                    shortest_paths[next_node] = (current_node, weight)
+
+        next_destinations = {node: shortest_paths[node] for node in shortest_paths if node not in visited}
+        if not next_destinations:
+            return "invalid path"
+        # next node is the destination with the lowest weight
+        current_node = min(next_destinations, key=lambda k: next_destinations[k][1])
+
+    # Work back through destinations in shortest path
+    path = []
+    while current_node is not None:
+        path.append(current_node)
+        next_node = shortest_paths[current_node][0]
+        current_node = next_node
+    # Reverse path
+    path = path[::-1]
+    return path
 
 
 # main game loop where player input is read
@@ -426,8 +463,9 @@ def game_loop():
         node.neighbours = dict.fromkeys(node.neighbours, random.randint(1, 3))
 
     draw_edges(graph)
-
     min_dist(machine, player, out)
+
+    # dijsktra(graph, machine, out)
 
     global stop_thread
     stop_thread = False
